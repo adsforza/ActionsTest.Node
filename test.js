a812a309cbc37d0ed51edb6d83d7100693d1baa4
adsforza <adsforza@hotmail.com>
2023-03-02 17:44:00 -0300

    initial commit

---
+++ test.js
@@ -0,0 +1,9 @@
+const assert = require('assert');
+describe('Simple Math Test', () => {
+    it('should return 2', () => {
+        assert.equal(1 + 1, 2);
+    });
+    it('Should return 9', () => {
+        assert.equal(3 * 3,9);
+    });
+});
